a21f75108423f886f56cb9f3937314c5e9dca3e3
Ghoul Mehdi <ghoulmehdi@MacBook-Pro-de-Ghoul.local>
2018-08-22 00:46:20 +0100

    finall Model of Concentration Brain

--- ConcentrationGM/ConcentrationGM/Controller/ConcentrationVC.swift
+++ ConcentrationGM/ConcentrationGM/Controller/ConcentrationVC.swift
@@ -11,6 +11,7 @@ import UIKit
 class ConcentrationVC: UIViewController {
     
     lazy var game = ConcentrationBR(numberOfPairsOfCardes: (groupOfCards.count + 1) / 2)
+  
     
     
 
@@ -26,9 +27,50 @@ class ConcentrationVC: UIViewController {
         flipCounter += 1
         if let cardNumber = groupOfCards.index(of: sender) {
             game.choosecard(by: cardNumber)
+            updateViewfromModel()
         }
+     }
+    func updateViewfromModel() {
+        for index in groupOfCards.indices {
+            let card = game.cards[index]
+            let button = groupOfCards[index]
+            if  card.isFaceUp {
+                button.setTitle( setemoji(for: card), for: UIControlState.normal)
+                button.backgroundColor =  #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+            } else {
+                button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0) : #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+                button.setTitle("", for: UIControlState.normal)
+               
+            }
+        }
+        
+    }
+   
+    
+    
+    
+    
+    var emojies = ["🧟‍♀️","🧛‍♂️","😈","👻","👹","👿","💀","🧟‍♂️","🧙🏼‍"]
+    
+    var emojieForCard = [Int:String]()
+    
+    func setemoji(for card: Card) -> String {
+        if emojieForCard[card.identifier] == nil, emojies.count > 0 {
+            let randomindec = Int(arc4random_uniform(UInt32(emojies.count)))
+            emojieForCard[card.identifier] = emojies.remove(at: randomindec)
+        }
+       return emojieForCard[card.identifier] ?? "?"
+        
     }
     
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
-
--- ConcentrationGM/ConcentrationGM/Model/ConcentrationBR.swift
+++ ConcentrationGM/ConcentrationGM/Model/ConcentrationBR.swift
@@ -9,15 +9,50 @@
 import Foundation
 class ConcentrationBR {
     var cards = [Card]()
+    
+    
+    var indexOfOneAndOnlyCardFaceUp : Int?
+    
     func choosecard(by index:Int){
+        if !cards[index].isMatched {
+            if let indexOfMatchingCard = indexOfOneAndOnlyCardFaceUp , indexOfMatchingCard != index {
+                if cards[index].identifier == cards[indexOfMatchingCard].identifier {
+                    cards[index].isMatched = true
+                    cards[indexOfMatchingCard].isMatched = true
+                    }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyCardFaceUp = nil
+            } else {
+                for faceDown in cards.indices {
+                    cards[faceDown].isFaceUp = false
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyCardFaceUp = index
+        }
+        }
+       }
+    
+    
+    
+    
+    
+    func shuffle(card : [Card]) -> [Card]{
+        var cards = card
+        var shuffledCard = [Card]()
+        for _ in 0..<cards.count {
+            let randomNumbre = Int(arc4random_uniform(UInt32(cards.count)))
+            shuffledCard.append(cards.remove(at: randomNumbre))
+        }; return shuffledCard
         
     }
-    
     
     init(numberOfPairsOfCardes : Int) {
         for _ in 0..<numberOfPairsOfCardes {
             let card = Card()
             cards += [card,card]
+            cards = shuffle(card: cards)
+        
+            
             }
         }
     
